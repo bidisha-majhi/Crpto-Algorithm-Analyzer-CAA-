4449fb50b323988b24b3d2d1bb1c823d328618bd
Shouvik Patra <shouvikpatra@gmail.com>
2021-04-11 01:44:17 +0530

    FIX: clean code

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-
-
+from algorithms.symmetric_algorithms import CryptographerAES
 
 
 if __name__ == "__main__":
-    print("Hello World")
+    aes_obj = CryptographerAES(256)
+    with open('data/original/someText.txt', 'rb') as f: 
+        encrypted = aes_obj.encrypt(f.read(), "shouvik")
+        with open('data/encrypted/encrypted.txt', 'wb') as e:
+            e.write(encrypted)
+    with open('data/encrypted/encrypted.txt', 'rb') as e:
+        decrypted = aes_obj.decrypt(e.read(), "shouvik")
+        with open('data/decrypted/decrypted.txt', 'wb') as d:
+            d.write(decrypted)
