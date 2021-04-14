3eeb915482df2a0b4fa0924abfa8a3ff52719049
Shouvik Patra <52216941+shouvikpatra@users.noreply.github.com>
2021-04-14 19:28:10 +0530

    ADD: Twofish Encryption (#5)
    * ADD: twofish initial implementation
    
    * ADD: Encryption for any block size

--- algorithms/symmetric_algorithms.py
+++ algorithms/symmetric_algorithms.py
@@ -3,6 +3,7 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 from base64 import b64encode, b64decode
 from hashlib import sha1, sha256
+from twofish import Twofish
 
 
 class CryptographerAES:
@@ -54,4 +55,61 @@ class CryptographerAES:
         return plain_text.decode() if is_string else plain_text
 
 
+class CryptographerTwofish:
+    
+    def __init__(self):
+        pass
 
+    def create_key(self, secret:str):
+        key = sha1(secret.encode()).hexdigest()
+        key = key[:30]
+        return key
+
+    def encrypt(self, plain_text, secret):
+        secret = "*"+self.create_key(secret)+"*"
+
+        is_string = False
+        if type(plain_text) == str:
+            is_string = True
+            plain_text = plain_text.encode('utf-8')
+
+        plain_text = pad(plain_text, 16)
+
+        secret = secret.encode('utf-8')
+        
+        cipher = Twofish(secret)
+        cipher_text = b""
+        for i in range(0, len(plain_text), 16):
+            plain_text_substring = plain_text[i: i+16]
+            cipher_text_substring = cipher.encrypt(plain_text_substring)
+            cipher_text_substring = b64encode(cipher_text_substring)
+            cipher_text += cipher_text_substring
+       
+        return cipher_text.decode() if is_string else cipher_text
+
+    def decrypt(self, cipher_text, secret):
+        secret = "*"+self.create_key(secret)+"*"
+        is_string = False
+        if type(cipher_text) == str:
+            is_string = True
+            cipher_text = cipher_text.encode('utf-8')
+        
+        secret = secret.encode('utf-8')
+        
+        cipher = Twofish(secret)
+        plain_text = b""
+        for i in range(0, len(cipher_text), 24):
+            cipher_text_substring = b64decode(cipher_text[i:i+24])
+            plain_text += cipher.decrypt(cipher_text_substring)
+        
+        plain_text = unpad(plain_text, 16)
+        
+        return plain_text.decode() if is_string else plain_text
+
+if __name__ == "__main__":
+    c = CryptographerTwofish()
+    plain_text  = "Hello World "*5
+    encrypted = CryptographerTwofish().encrypt(plain_text=plain_text, secret='shouvikshouvikshouvikshouviksh')
+    print(encrypted)
+    decrypted = CryptographerTwofish().decrypt(cipher_text=encrypted, secret='shouvikshouvikshouvikshouviksh')
+    print(decrypted)
