c1dc4d8fe0e4fcb965100f36254e7cedb9f02c36
Bidisha <mbidisha10@gnail.com>
2021-04-17 19:41:30 +0530

    Added SHA1, SHA224, SHA256, SHA384, SHA512  Hash functions

--- algorithms/hash_functions.py
+++ algorithms/hash_functions.py
@@ -64,7 +64,81 @@ class CryptographerWHIRLPOOL:
         cipher.update(plain_text)
         cipher_text = cipher.hexdigest()
         return cipher_text
+   
+class CryptographerSHA1():
+  
+  def __init__(self):
+    pass
+  
+  def encrypt(self,plain_text):
+    if type(plain_text) == str:
+      plain_text = plain_text.encode('utf-8')
+      msg = hashlib.sha1(plain_text)
+      return (msg.hexdigest())
 
+      
+class CryptographerSHA224():
+  
+  def __init__(self):
+    pass
+  
+  def encrypt(self,plain_text):
+    if type(plain_text) == str:
+      plain_text = plain_text.encode('utf-8')
+      msg = hashlib.sha224(plain_text)
+      return (msg.hexdigest())
+      
+      
+class CryptographerSHA256():
+  
+  def __init__(self):
+    pass
+  
+  def encrypt(self,plain_text):
+    is_string = False
+    if type(plain_text) == str:
+      plain_text = plain_text.encode('utf-8')
+      msg = hashlib.sha256(plain_text)
+      return (msg.hexdigest())
+      
+      
+class CryptographerSHA384():
+  
+  def __init__(self):
+    pass
+  
+  def encrypt(self,plain_text):
+    if type(plain_text) == str:
+      plain_text = plain_text.encode('utf-8')
+      msg = hashlib.sha384(plain_text)
+      return (msg.hexdigest())
+      
+      
+class CryptographerSHA512():
+  
+  def __init__(self):
+    pass
+  
+  def encrypt(self,plain_text):
+    if type(plain_text) == str:
+      plain_text = plain_text.encode('utf-8')
+      msg = hashlib.sha512(plain_text)
+      return (msg.hexdigest())
+      
+    
+    
 if __name__ == "__main__":
+
+    plain_text = 'Bidisha2306'
+    encrypted1 = CryptographerSHA1().encrypt(plain_text=plain_text)
+    print(encrypted1)
+    encrypted2 = CryptographerSHA224().encrypt(plain_text=plain_text)
+    print(encrypted2)
+    encrypted3 = CryptographerSHA256().encrypt(plain_text=plain_text)
+    print(encrypted3)
+    encrypted4 = CryptographerSHA384().encrypt(plain_text=plain_text)
+    print(encrypted4)
+    encrypted5 = CryptographerSHA512().encrypt(plain_text=plain_text)
+    print(encrypted5)    
     print(hashlib.algorithms_available)
     print(CryptographerWHIRLPOOL().encrypt("Shouvik"))
