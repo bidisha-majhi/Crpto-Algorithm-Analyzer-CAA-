dd703f05125a3b3b564e9c4eb3fee3265210d8bf
Shouvik Patra <77960514+shouvikpatra-jm@users.noreply.github.com>
2021-05-17 03:15:01 +0530

    ADD: main.py |FIX: minor bugs and optimisations (#11)

--- main.py
+++ main.py
@@ -1,36 +1,91 @@
-from algorithms.symmetric_algorithms import CryptographerAES
+import os
+from algorithms.symmetric_algorithms import *
 from algorithms.hash_functions import *
 from utils.consumption import ConsumptionMeasure
+from utils.csv_generator import CSVGenerator
+
+ORIGINAL_FILE_BASE_PATH = "data/original/"
+ENCRYPTED_FILE_BASE_PATH = "data/encrypted/"
+DECRYPTED_FILE_BASE_PATH = "data/decrypted/"
+
+ALGORITHM = CryptographerSHA224()
+FILE_NAME = "someText.txt"
+MODE = "HASHING"
+PASSWORD = "Shouvik"
+FILE_INPUT_PATH = (ORIGINAL_FILE_BASE_PATH if MODE.lower() != "decryption" else ENCRYPTED_FILE_BASE_PATH) + FILE_NAME
+FILE_SIZE = os.path.getsize(FILE_INPUT_PATH)
+
+#CONSUMPTION MEASURE PARAMETERS
+EPOCH = 2*20
+
+#CSV GENERATOR PARAMETERS
+PREVIOUS_OUTPUT = "Results_Symmetric_Algorithms.csv" if MODE.lower() != "hashing" else "Results_Hashing_Functions.csv"
+OUTPUT = "Results_Symmetric_Algorithms.csv" if MODE.lower() != "hashing" else "Results_Hashing_Functions.csv"
+INDEX = None
 
 if __name__ == "__main__":
+    crypt_obj = ALGORITHM
+
+    pre, ext = os.path.splitext(FILE_NAME)
+
+    print(f"Reading File {FILE_NAME} for {MODE.title()}...")
+    with open(FILE_INPUT_PATH, "rb") as f:
+        original_file = f.read()
+    print(f"Successfully Read File {FILE_NAME} for {MODE.title()}\n")
+
+    print(f"Performing {MODE.title()} with EPOCH {EPOCH} with Consumption Measure...")
     consumption_measure = ConsumptionMeasure()
-    crypt_obj = CryptographerAES(256)
-
-    original_file = open("data/original/someText.txt", "rb").read()
-
-    print("ENCRYPTION")
-    encrypted = consumption_measure.measure(
-        lambda: crypt_obj.encrypt(original_file, "shouvik"),
-        epoch=2*20
+    if MODE.lower() == "encryption":
+        func = lambda: crypt_obj.encrypt(original_file, PASSWORD)
+    elif MODE.lower() == "decryption":
+        func = lambda: crypt_obj.decrypt(original_file, PASSWORD)
+    else:
+        func = lambda: crypt_obj.encrypt(original_file)
+    file_output = consumption_measure.measure(
+        func,
+        epoch=EPOCH
     )
-    with open('data/encrypted/encrypted.txt', 'wb') as e:
-        e.write(encrypted)
-    print("Energy Consumption (in micro Joules)", consumption_measure.get_energy_consumption())
-    print("Time Taken (in microseconds)", consumption_measure.get_duration())
+    print(f"{MODE.title()} Completed Successfully with Consumption Measure\n")
 
-    encrypted = open("data/encrypted/encrypted.txt", "rb").read()
-    print("\n\nDECRYPTION")
-    decrypted = consumption_measure.measure(
-        lambda: crypt_obj.decrypt(encrypted, "shouvik"),
-        epoch=2*20
+    if MODE.lower() != "hashing":
+        print(f"Saving {MODE.title()} Output...")
+        file_output_path = ENCRYPTED_FILE_BASE_PATH+FILE_NAME if MODE.lower() == "encryption" else DECRYPTED_FILE_BASE_PATH+FILE_NAME
+        with open(file_output_path, 'wb') as f:
+            f.write(file_output)
+        print(f"Successfully saved {MODE.title()} Output to File: {file_output_path}\n")
+    else:
+        print(f"Hash Generated: {file_output}\n")
+
+    print(f"\nSaving Consumption Measure Data...")
+    result = CSVGenerator(output_csv=PREVIOUS_OUTPUT)
+
+    algorithm = (type(ALGORITHM).__name__.replace("Cryptographer", "") + " " + str(ALGORITHM.__dict__.get('type', ""))).upper().strip()
+    file_name = FILE_NAME
+    file_size = FILE_SIZE
+    mode = MODE.upper()
+    duration = consumption_measure.get_duration()
+    energy_consumed = consumption_measure.get_energy_consumption()
+
+    result.add_or_update_value(
+        algorithm=algorithm,
+        file_name=file_name,
+        file_size=file_size,
+        mode=mode,
+        duration=duration,
+        energy_consumed=energy_consumed,
+        index=INDEX
     )
-    with open('data/decrypted/decrypted.txt', 'wb') as d:
-        d.write(decrypted)
+    result.reset_index()
+    result.save_csv(file_name=OUTPUT)
 
-    print("Energy Consumption (in micro Joules)", consumption_measure.get_energy_consumption())
-    print("Time Taken (in microseconds)", consumption_measure.get_duration())
+    print("")
+    print("ALGORITHM:", algorithm)
+    print("FILE NAME:", file_name)
+    print("FILE SIZE (in bytes):", file_size)
+    print("MODE:", mode)
+    print("DURATION (in microseconds): ", duration)
+    print("ENERGY CONSUMED (in micro Joules)", consumption_measure.get_energy_consumption())
+    print("")
 
-    a = original_file
-    b = open('data/decrypted/decrypted.txt', 'rb').read()
-    from hashlib import sha1
-    print(sha1(a).hexdigest() == sha1(b).hexdigest())
+    print("Sucessfully Saved Consumption Measure Data.")
+
